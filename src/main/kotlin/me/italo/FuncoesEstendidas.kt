e7d578918393c5855bc8a9ba5160899870741b07
Italo Augusto <italo.augusto.ct@gmail.com>
2021-08-27 16:26:14 -0300

    Created extension functions test file

---
+++ src/main/kotlin/me/italo/FuncoesEstendidas.kt
@@ -0,0 +1,14 @@
+package me.italo
+
+import java.math.BigDecimal
+
+fun Array<BigDecimal>.somatoria() = this.reduce{ acc, valor ->
+    acc + valor
+}
+
+fun Array<BigDecimal>.media() =
+    if(this.isEmpty()) {
+        BigDecimal.ZERO
+    } else {
+        this.somatoria() / this.size.toBigDecimal()
+    }
---
+++ src/main/kotlin/me/italo/TesteFuncoesEstendidas.kt
@@ -0,0 +1,11 @@
+package me.italo
+
+fun main(){
+    val salarys = arrayOf("2000".toBigDecimal(), "1500".toBigDecimal(), "4000".toBigDecimal())
+
+    println(salarys.somatoria())
+    println("-----------")
+
+    println(salarys.media())
+
+}
